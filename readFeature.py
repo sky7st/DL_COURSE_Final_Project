a983d74c466e0fca72375a5efe3d27e24719e0c5
neil <s1k2y37st@gmail.com>
2021-01-08 15:08:56 +0800

    first commit

---
+++ readFeature.py
@@ -0,0 +1,85 @@
+import cv2, os, sys, time, h5py
+import mediapipe as mp
+import numpy as np
+
+label_basics = ["VirtualMouse", "VirtualSelect", "Other"]
+
+filename = "data.h5"
+
+labels = []
+show_data_index = 0
+show_key_index = 0
+
+cam_h = 480
+cam_w = 640
+
+def drawData(data):
+    empty_img = np.zeros((cam_h, cam_w, 3), dtype=np.float)
+    for i in range(data.shape[0]):
+        x = int(data[i][0] * cam_w)
+        y = int(data[i][1] * cam_h)
+        empty_img = cv2.circle(empty_img, (x, y), radius=0, color=(0, 0, 255), thickness=4)
+    return empty_img
+
+with h5py.File(filename, 'r') as h5f:
+    labels = list(h5f.keys())
+    max_label_index = len(labels) - 1
+
+    label = labels[show_key_index]
+    dataset = h5f[label][()]
+
+    max_data_index = dataset.shape[0] - 1
+
+    data = dataset[show_data_index]
+    img = drawData(data)
+    cv2.putText(img, "index: {}".format(show_data_index), (10, 30), 0, 1, (255, 0, 0))
+    cv2.putText(img, "label: {}".format(label), (10, 55), 0, 1, (255, 0, 0))
+    cv2.putText(img, "shape:{}".format(data.shape), (10, 80), 0, 1, (255, 0, 0))
+    cv2.imshow('MediaPipe Hands', img)
+    while True:
+        try:
+            key =  cv2.waitKeyEx(5)
+            if key == 2490368 or key == 2621440:
+                if key == 2490368: #ARROW_UP
+                    if show_key_index >= max_label_index:
+                        show_key_index = 0
+                    else:
+                        show_key_index += 1
+                elif key == 2621440: #ARROW_DOWN
+                    if show_key_index <= 0:
+                        show_key_index = max_label_index
+                    else:
+                        show_key_index -= 1
+                label = labels[show_key_index]
+                dataset = h5f[label][()]
+                max_data_index = dataset.shape[0] - 1
+                show_data_index = 0
+                data = dataset[show_data_index]
+                img = drawData(data)
+                cv2.putText(img, "index: {}".format(show_data_index), (10, 30), 0, 1, (255, 0, 0))
+                cv2.putText(img, "label: {}".format(label), (10, 55), 0, 1, (255, 0, 0))
+                cv2.putText(img, "shape:{}".format(data.shape), (10, 80), 0, 1, (255, 0, 0))
+                cv2.imshow('MediaPipe Hands', img)
+            elif key == 2424832 or key == 2555904:
+                if key == 2424832: #ARROW_LEFT
+                    if show_data_index <= 0:
+                        show_data_index = max_data_index
+                    else:
+                        show_data_index -= 1
+                elif key == 2555904: #ARROW_RIGHT
+                    if show_data_index >= max_data_index:
+                        show_data_index = 0
+                    else:
+                        show_data_index += 1
+                data = dataset[show_data_index]
+                img = drawData(data)
+                cv2.putText(img, "index: {}".format(show_data_index), (10, 30), 0, 1, (255, 0, 0))
+                cv2.putText(img, "label: {}".format(label), (10, 55), 0, 1, (255, 0, 0))
+                cv2.putText(img, "shape:{}".format(data.shape), (10, 80), 0, 1, (255, 0, 0))
+                cv2.imshow('MediaPipe Hands', img)
+            else:
+                if key == 27:
+                    break
+        except KeyboardInterrupt:
+            break
+
---
+++ testFeature.py
@@ -0,0 +1,77 @@
+import cv2, os, sys, time, h5py
+import mediapipe as mp
+import numpy as np
+
+####my label param####
+
+label_basics = ["VirtualMouse", "VirtualSelect", "Other"]
+# label_name = "VirtualMouse"  ## 2 finger
+# label_name = "VirtualSelect"   ## 1 finger
+label_name = label_basics[2]            ## other (background)
+
+save_path = "Dataset/"
+######################
+
+full_save_path = save_path + label_name + "/"
+
+####file save pre#####
+if not (os.path.isdir(full_save_path)):
+    os.mkdir(full_save_path)
+
+######################
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# For webcam input:
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.7, 
+    min_tracking_confidence=0.3
+)
+cap = cv2.VideoCapture(0)
+all_frame_features = []
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+        continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    one_frame_feature = []
+    if results.multi_hand_landmarks:
+        # for hand_landmarks in results.multi_hand_landmarks:
+        hand_landmarks = results.multi_hand_landmarks[0]
+        mp_drawing.draw_landmarks(image, hand_landmarks)
+        
+        for i, data_point in enumerate(hand_landmarks.landmark):
+            one_frame_feature.append(
+                np.array([data_point.x, data_point.y, data_point.z])
+            )
+        one_frame_feature = np.array(one_frame_feature)
+        all_frame_features.append(one_frame_feature)
+    print(len(all_frame_features))
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+hands.close()
+cap.release()
+
+all_frame_features = np.array(all_frame_features)
+shape = all_frame_features.shape
+dataset_name = "{}_{}".format(label_name, int(time.time()))
+
+with h5py.File('data.h5', 'a') as h5f:
+    h5f.create_dataset(dataset_name, data=all_frame_features)
