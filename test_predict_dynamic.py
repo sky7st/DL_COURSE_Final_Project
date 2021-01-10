07756301990c1f1a40a783e95c4ae1a3b4ea6335
s1k2y37st <s1k2y37st@gmail.com>
2021-01-10 23:29:32 +0800

    add predict plot

--- test_predict_dynamic.py
+++ test_predict_dynamic.py
@@ -3,11 +3,14 @@ import mediapipe as mp
 import numpy as np
 import tensorflow as tf
 from utils import *
-import queue
+
+import matplotlib.pyplot as plt
 
 labels = ['MouseLeft', 'MouseMove', 'MouseRight']
 
-model = tf.keras.models.load_model('model_dynamic_mouse.h5')
+avg_cnt = 40
+
+model = tf.keras.models.load_model('model_dynamic_mouse_hs.h5')
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -17,23 +20,50 @@ hands = mp_hands.Hands(
     static_image_mode=False,
     max_num_hands=1,
     min_detection_confidence=0.7, 
-    min_tracking_confidence=0.3
+    min_tracking_confidence=0.7
 )
 
-cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+# cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+cap = cv2.VideoCapture("test_dynamic.mkv")
+
+cap_type = cap.get(cv2.CAP_PROP_BACKEND)
+
 frame_cnt = 0
+feature_cnt = 0
+predict_cnt = 0
+none_feature_cnt = 0
+
+
 total_frame = []
+result_ls = []
+result_0, result_1, result_2 = [], [], []
+conv_fifo_left, conv_fifo_right = [], []
+
+
+first_draw = False
+
+fig = plt.figure(figsize=(12, 6))
+ax = plt.axes()
+ax.set_ylim([-0.1, 1.1])
+plt.legend()
+plt.xlabel("Feature Frame Cnt")
+plt.ylabel("Prediction")
+
 while cap.isOpened():
     success, image = cap.read()
     if not success:
-        print("Ignoring empty camera frame.")
-        # If loading a video, use 'break' instead of 'continue'.
-        continue
+        if cap_type == cv2.CAP_DSHOW:
+            print("Ignoring empty camera frame.")
+            continue
+        else:
+            print("video end")
+            break
 
-    
     # Flip the image horizontally for a later selfie-view display, and convert
     # the BGR image to RGB.
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # if cap_type == cv2.CAP_DSHOW:
+    image = cv2.flip(image, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
@@ -57,21 +87,36 @@ while cap.isOpened():
         one_frame_feature = np.array(one_frame_feature)
         one_frame_feature_shift = calShift(one_frame_feature)
         drawData(one_frame_feature_shift, image)
-        if len(total_frame) < 50:
+        if len(total_frame) < avg_cnt:
             total_frame.append(one_frame_feature_shift)
         else:
             total_frame.pop(0)
             total_frame.append(one_frame_feature_shift)
 
-        if len(total_frame) == 50:
+        if len(total_frame) == avg_cnt:
             total_frame_np = np.array(total_frame)
             x_test = np.expand_dims(total_frame_np, axis=0)
 
             result = model.predict(x_test)
+            result_ls.append(result[0])
+
+            result_0.append(result[0][0])
+            result_1.append(result[0][1])
+            result_2.append(result[0][2])
+
             result_index = result[0].argmax()
+            # if result[0][result_index] < 0.6:
+            #     result_index = 1
 
             cv2.putText(image, "Result:{} {}".format(result_index, labels[result_index]), (10, 30), 1, 2, (0, 0, 255), 2)
+            predict_cnt += 1
 
+        feature_cnt += 1
+        
+    else:
+        none_feature_cnt += 1
+        if none_feature_cnt > 15:
+            total_frame = []
 
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
@@ -79,3 +124,12 @@ while cap.isOpened():
 
 hands.close()
 cap.release()
+
+
+### draw plot
+ax.plot(result_0, color='blue', label='left click') 
+ax.plot(result_1, color='black', label='moving')
+ax.plot(result_2, color='red', label='right click')
+
+plt.show()
+# plt.close(fig)
